524c00e74751f6e297041374751c1408852de47c
Kacper Smietana <kabox9@gmail.com>
2023-05-14 21:56:45 +0200

    crew

---
+++ src/js/crew.js
@@ -0,0 +1,7 @@
+const crewImg = document.querySelector('.crew-img')
+const jobTitle = document.querySelector('.job-title')
+const fullName = document.querySelector('.full-name')
+const personDesc = document.querySelector('.person-desc')
+
+const dots = document.querySelectorAll('.dot')
+
