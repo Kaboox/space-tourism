4f0a0207c411866b2e3f3add3f63b3eb390a9559
Kacper Smietana <kabox9@gmail.com>
2023-05-16 12:08:28 +0200

    another img ext

--- src/js/crew.js
+++ src/js/crew.js
@@ -8,7 +8,7 @@ const circles = document.querySelectorAll('.circle')
 
 
 const firstPerson = {
-    imgPath: '../../dist/img/crew/image-douglas-hurley.webp',
+    imgPath: '../../dist/img/crew/image-douglas-hurley.png',
     jobTitle: 'Commander',
     fullName: 'Douglas Hurley',
     personDesc: `Douglas Gerald Hurley is an American engineer, former Marine Corps pilot 
@@ -16,7 +16,7 @@ const firstPerson = {
     commander of Crew Dragon Demo-2.`
 }
 const secondPerson = {
-    imgPath: '../../dist/img/crew/image-victor-glover.webp',
+    imgPath: '../../dist/img/crew/image-victor-glover.png',
     jobTitle: 'Pilot',
     fullName: 'Victor Glover',
     personDesc: `Pilot on the first operational flight of the SpaceX Crew Dragon to the 
@@ -25,7 +25,7 @@ const secondPerson = {
     station systems flight engineer. `
 }
 const thirdPerson = {
-    imgPath: '../../dist/img/crew/image-mark-shuttleworth.webp',
+    imgPath: '../../dist/img/crew/image-mark-shuttleworth.png',
     jobTitle: 'Mission Specialist',
     fullName: 'Mark Shuttleworth',
     personDesc: `Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind 
@@ -34,7 +34,7 @@ const thirdPerson = {
 }
 
 const fourthPerson = {
-    imgPath: '../../dist/img/crew/image-anousheh-ansari.webp',
+    imgPath: '../../dist/img/crew/image-anousheh-ansari.png',
     jobTitle: 'Flight Engineer',
     fullName: 'Anousheh Ansari',
     personDesc: `Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. 
